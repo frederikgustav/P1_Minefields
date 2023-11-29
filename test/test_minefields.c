31a9721ebfdd42913fdfa4779fc79dc3f2bbb651
fredgusbach <120267919+fredgusbach@users.noreply.github.com>
2023-11-29 13:13:39 +0100

    add tests

--- test/test_minefields.c
+++ test/test_minefields.c
@@ -1,7 +1,28 @@
 #include <stdlib.h>
+#include <assert.h>
 
-#include "..\src\lib.h"
+#include "..\src\minelib.h"
+
+void test_get_empty_minefield();
+void test_get_random_minefield();
 
 int main(void) {
+    test_get_empty_minefield();
+
     return EXIT_SUCCESS;
+}
+
+void test_get_empty_minefield() {
+    struct minefield field = get_empty_minefield(5, 5, 1);
+
+    for (int y = 0; y < field.height; ++y) {
+        for (int x = 0; x < field.width; ++x) {
+            assert(field.matrix[y][x].mine == 0);
+        }
+    }
+}
+
+void test_get_random_minefield() {
+    struct minefield field = get_random_minefield(5, 5, 1, 5);
+
 }
