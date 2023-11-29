f7fd55e87672c0ce61d0dada7753aaf19e3e6220
Elma Vukicevic <HW00GZ@student.aau.dk>
2023-11-29 18:36:01 +0100

    uni test elma med random og empty minefield

--- test/test_minefields.c
+++ test/test_minefields.c
@@ -8,13 +8,23 @@ void test_get_random_minefield();
 
 int main(void) {
     test_get_empty_minefield();
+    test_get_random_minefield();
 
     return EXIT_SUCCESS;
 }
 
 void test_get_empty_minefield() {
-    struct minefield field = get_empty_minefield(5, 5, 1);
+    /* Arrange */
+    int width = 5;
+    int height = 5;
+    int metric_square_length = 1;
 
+    /* Act */
+    struct minefield field = get_empty_minefield(width, height, metric_square_length);
+
+    /* Assert */
+    // Nested for-loop to check matrix positions for mines
+    // "== 0" to check that no mine is in [x][y] position
     for (int y = 0; y < field.height; ++y) {
         for (int x = 0; x < field.width; ++x) {
             assert(field.matrix[y][x].mine == 0);
@@ -23,14 +33,27 @@ void test_get_empty_minefield() {
 }
 
 void test_get_random_minefield() {
-    // Arrange
+    /* Arrange */
+    //Variables for Act
     int width = 5;
     int height = 5;
+    int metric_square_length = 1;
     int amount = 5;
 
-    // Act
-    struct minefield field = get_random_minefield(width, height, 1, amount);
+    //Variable for Assert
+    int assert_counter = 0;
 
+    /* Act */
+    struct minefield field = get_random_minefield(width, height, metric_square_length, amount);
 
+    for (int y = 0; y < field.height; ++y) {
+        for (int x = 0; x < field.width; ++x) {
+            if (field.matrix[y][x].mine == 1){
+                ++assert_counter;
+            }
+        }
+    }
 
+    /* Assert */
+    assert(assert_counter == amount);
 }
