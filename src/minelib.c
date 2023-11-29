ed3377d4e1ae73e0e86876cbef65eac6c55f33fb
niixghty <94524874+niixghty@users.noreply.github.com>
2023-11-29 15:23:07 +0100

    added comments

--- src/minelib.c
+++ src/minelib.c
@@ -3,11 +3,17 @@
 #include <stdio.h>
 #include <time.h>
 
+/**
+ *
+ * Takes a width, height and metric_square_length, referring to the metric length of a square,
+ * and returns an empty minefield.
+ * @param width the width of the empty minefield
+ * @param height the height of the empty minefield
+ * @param metric_square_length the size of the squares in the metric
+ * @return the function returns a struct representing an empty minefield with the specified
+ * dimensions and square length, where all squares initially have no mines.
+ */
 struct minefield get_empty_minefield(int width, int height, double metric_square_length) {
-    /*
-     * Takes a width, height and metric_square_length, referring to the metric length of
-     * a square, and returns an empty minefield.
-     */
     struct minefield field;
 
     field.height = height;
@@ -25,11 +31,17 @@ struct minefield get_empty_minefield(int width, int height, double metric_square
     return field;
 }
 
+/**
+ * Takes a width, height, mine_amount and metric_square_length, referring to the metric length of
+ * a square, and returns a minefield with randomly placed mines.
+ * @param width the width of the empty minefield with mines in it
+ * @param height the height of the empty minefield with mines in it
+ * @param metric_square_length the size of the squares in the metric
+ * @param mine_amount the amount of mines in the minefields
+ * @return the function returns a minefield with randomly placed mines based on the specified dimensions,
+ * square length, and the number of mines to be placed.
+ */
 struct minefield get_random_minefield(int width, int height, double metric_square_length, int mine_amount) {
-    /*
-     * Takes a width, height, mine_amount and metric_square_length, referring to the metric length of
-     * a square, and returns a minefield with randomly placed mines.
-     */
     struct minefield field = get_empty_minefield(width, height, metric_square_length);
     srand(time(NULL));
 
@@ -49,13 +61,15 @@ struct minefield get_random_minefield(int width, int height, double metric_squar
     return field;
 }
 
+/**
+ * Prints a minefield, example:
+ * | O O X |
+ * | X O 0 |
+ * | O O X |
+ * @param field this parameter is used to access information about the minefield and print its contents
+ * it prints the minefield as 0 and xs
+ */
 void print_minefield(struct minefield field) {
-    /*
-     * Prints a minefield, example:
-     * | O O X |
-     * | X O 0 |
-     * | O O X |
-     */
     for (int y = 0; y < field.height; ++y) {
         printf("| ");
 
@@ -71,20 +85,33 @@ void print_minefield(struct minefield field) {
     }
 }
 
+/**
+ *
+ * @param field Takes a minefield, and deallocates the memory
+ */
 void free_minefield(struct minefield field) {
-    /*
-     * Takes a minefield, and deallocates the memory.
-     */
     for (int y = 0; y < field.height; ++y) {
         free(field.matrix[y]);
     }
     free(field.matrix);
 }
 
+/**
+ *
+ * @param a
+ * @param b
+ * @return it returns the minimum of a and b. If a is less than b, it returns a; otherwise, it returns b.
+ */
 int min(int a, int b) {
     return (a < b) ? a : b;
 }
 
+/**
+ *
+ * @param field the parameter field is used to provide information about the generated minefield for this function
+ * to find the largest possible area within the minefield that can be cleared (that has as many mines as possible)
+ * @return it retures the area that contains the most mines
+ */
 struct sub_minefield get_biggest_cleared_sub_minefield(struct minefield field) {
     int max_size = 0;
     int max_y = 0, max_x = 0;
