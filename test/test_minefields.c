4ca62c098d3f6ad149e4175d0deef9794d3259a6
fredgusbach <120267919+fredgusbach@users.noreply.github.com>
2023-12-04 10:59:44 +0100

    added more tests

--- test/test_minefields.c
+++ test/test_minefields.c
@@ -6,6 +6,7 @@
 void test_get_empty_minefield();
 void test_get_random_minefield();
 void test_get_zone_mine_sum();
+void test_get_zone_area();
 
 int main(void) {
     // Seed random generator
@@ -14,6 +15,7 @@ int main(void) {
     test_get_empty_minefield();
     test_get_random_minefield();
     test_get_zone_mine_sum();
+    test_get_zone_area();
 
     return EXIT_SUCCESS;
 }
@@ -61,24 +63,56 @@ void test_get_random_minefield() {
     assert(assert_counter == amount);
 }
 
+
 void test_get_zone_mine_sum() {
     /* Arrange */
-    zone zone = {{0,0}, {1,1}};
-    minefield field = get_empty_minefield(5, 5);
+    //Variables for Act
+    int width = 5;
+    int height = 5;
+    minefield field;
 
-    // Inside zone
-    field.matrix[0][0].mine = 1;
-    field.matrix[0][1].mine = 1;
-    field.matrix[1][0].mine = 1;
-    field.matrix[1][1].mine = 1;
+    // Initialize the minefield matrix with no mines
+    field.height = height;
+    field.width = width;
+    for (int y = 0; y < field.height; ++y) {
+        for (int x = 0; x < field.width; ++x) {
+            field.matrix[y][x].mine = 0;
+        }
+    }
 
-    // Outside zone
-    field.matrix[2][2].mine = 1;
+    // Place mines at specific positions
+    field.matrix[1][2].mine = 1;
+    field.matrix[2][3].mine = 1;
+    field.matrix[3][1].mine = 1;
     field.matrix[3][3].mine = 1;
+    field.matrix[4][4].mine = 1;
+
+    zone biggest_rect;
+    biggest_rect.start.x = 1;
+    biggest_rect.start.y = 1;
+    biggest_rect.end.x = 4;
+    biggest_rect.end.y = 4;
 
     /* Act */
-    int area = get_zone_mine_sum(field, zone);
+    int result = get_zone_mine_sum(field, biggest_rect);
 
     /* Assert */
-    assert(area == 4);
+    assert(result == 5);  // Adjust the expected value based on your minefield matrix and zone
+}
+
+void test_get_zone_area() {
+    /* Arrange */
+    //Variables for Act
+    zone testZone;
+    testZone.start.x = 1;
+    testZone.start.y = 1;
+    testZone.end.x = 4;
+    testZone.end.y = 4;
+
+    /* Act */
+    int result = get_zone_area(testZone);
+
+    /* Assert */
+    //Calculate the area manually to check if function calculates it correctly. (hvorfor +1 hvis det alligevel giver 4?)
+    assert(result == 16);
 }
