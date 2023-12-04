b946d7818541ff0637f07120162941c0576163c0
fredgusbach <fredgusbach@gmail.com>
2023-12-04 23:22:15 +0100

    add demo or experiment choice

--- src/main.c
+++ src/main.c
@@ -5,10 +5,30 @@
 #include "minefield_algorithms/minefield_algorithms.h"
 
 int main(void) {
-    // begin experiment
-    srand(time(NULL));
+    // let user choose between running demo or experiments
+    char choice;
+    do {
+        printf("Run demo or experiments? (d/e): \n");
+        scanf("%c", &choice);
+    } while (choice != 'd' && choice != 'e');
 
-    mulitiple_experiment_runs(5, 5, 20, 1);
+    if (choice == 'd') {
+        // run demo
+        printf("Run demo\n");
+    } else {
+        // run experiments
+        int width, height, mine_count, runs;
+        printf("Enter width:\n");
+        scanf("%d", &width);
+        printf("Enter height:\n");
+        scanf("%d", &height);
+        printf("Enter mine count:\n");
+        scanf("%d", &mine_count);
+        printf("Enter number of runs:\n");
+        scanf("%d", &runs);
+
+        mulitiple_experiment_runs(width, height, mine_count, runs);
+    }
 
     return EXIT_SUCCESS;
 }
