c57019d21de00f04f5ca3caaa694a0f2527e3b41
fredgusbach <fredgusbach@gmail.com>
2023-12-11 17:09:28 +0100

    removed demo choice from entry point as this isnt in scope currently

--- src/main.c
+++ src/main.c
@@ -5,30 +5,21 @@
 #include "minefield_algorithms/minefield_algorithms.h"
 
 int main(void) {
-    // let user choose between running demo or experiments
-    char choice;
-    do {
-        printf("Run demo or experiments? (d/e): \n");
-        scanf("%c", &choice);
-    } while (choice != 'd' && choice != 'e');
+    // set random seed
+    srand(time(NULL));
 
-    if (choice == 'd') {
-        // run demo
-        printf("Run demo\n");
-    } else {
-        // run experiments
-        int width, height, mine_count, runs;
-        printf("Enter width:\n");
-        scanf("%d", &width);
-        printf("Enter height:\n");
-        scanf("%d", &height);
-        printf("Enter mine count:\n");
-        scanf("%d", &mine_count);
-        printf("Enter number of runs:\n");
-        scanf("%d", &runs);
+    // run experiments
+    int width, height, mine_count, runs;
+    printf("Enter width:\n");
+    scanf("%d", &width);
+    printf("Enter height:\n");
+    scanf("%d", &height);
+    printf("Enter mine count:\n");
+    scanf("%d", &mine_count);
+    printf("Enter number of runs:\n");
+    scanf("%d", &runs);
 
-        multiple_experiment_runs(width, height, mine_count, runs, 1, 1, 1000);
-    }
+    multiple_experiment_runs(width, height, mine_count, runs, 0, 1, 1);
 
     return EXIT_SUCCESS;
 }
